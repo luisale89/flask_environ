a4d7610379e132564e3cca8469a9b43aab3a59fe
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2020-08-09 21:05:37 +0000

    changes: migrations
    Se configura app para realizar migraciones a bd utilizando postgresql.
    Esta app es un modelo que podrá ser replicado para distintos tipos de
    aplicaciones, como API o web pages.
    La ventaja es que cuenta de forma integrada con unabase de datos potente.
    en el servidor se deberá configurar la variable de entorno respectiva.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify, url_for
+from flask_migrate import Migrate
+from models import db, User
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+MIGRATE = Migrate(app, db)
+db.init_app(app)
+
 
 @app.route("/")
 def home():
-    return '<h1>Hello World</h1>'
+    response_body = {
+        "msg": "Hola a Todos, JSON msg"
+    }
 
+    return jsonify(response_body), 200
+
+@app.route("/webview")
+def web():
+    return '<h1>Hola a todos. Vista WEB del Sitio</h1>'
 
 if __name__ == '__main__':
-    app.run()
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
